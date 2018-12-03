933bdd4b89009dfb3a83b15c6d647695e2c4c009
Zach Capshaw <zcapshaw@gmail.com>
2018-12-02 18:30:54 -0700

    Create JSON data source for component

--- components/HorizontalCommentaries.js
+++ components/HorizontalCommentaries.js
@@ -6,32 +6,35 @@ import {
   View,
   ImageBackground
 } from 'react-native';
+import data from '../constants/CoreTruths.json';
 
 export default class HorizontalCommentaries extends Component {
+  constructor(props) {
+		super(props);
+		this.state = {
+			data,
+		};
+	}
 
   _renderItem = ({ item }) => (
     <ImageBackground
       source={{ uri: 'https://images.unsplash.com/photo-1534608176107-b67f671733b3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2eaaaa01848d4a9c31b21a3b9165af6f&auto=format&fit=crop&w=1052&q=80' }}
       style={styles.item}
     >
-      <Text style={styles.itemText}>{item.key}</Text>
+      <Text style={styles.itemText}>{item.shortname}</Text>
     </ImageBackground>
   );
 
   render() {
+    console.log(this.state)
     return (
       <View style={[styles.contentContainer, styles.commentariesContainer]}>
         <FlatList
-          data={[
-            { key: 'First\nCore Truth' },
-            { key: 'Second\nCore Truth' },
-            { key: 'Third\nCore Truth' },
-            { key: 'Fourth\nCore Truth' },
-            { key: 'Fifth\nCore Truth' },
-        ]}
+          data={this.state.data}
           horizontal
           showsHorizontalScrollIndicator={false}
           renderItem={this._renderItem}
+          keyExtractor={(item, index) => index.toString()}
         />
       </View>
     );
